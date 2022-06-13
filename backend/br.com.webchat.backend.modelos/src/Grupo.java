bcdc2b3a4f5379a59eaf5868ad754c68f62c7765
JackOBryan <106849124+JackOBryan@users.noreply.github.com>
2022-06-12 21:35:11 -0300

    Add files via upload

---
+++ backend/br.com.webchat.backend.modelos/src/Grupo.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class Grupo {
+	ArrayList<Usuarios> usuarios = new ArrayList<>();
+
+}
---
+++ backend/br.com.webchat.backend.modelos/src/Usuarios.java
@@ -0,0 +1,35 @@
+
+public class Usuarios {
+	
+	private String nome;
+	private String Sobrenome;
+	private String Login;
+	private String senha;
+	
+	public String getLogin() {
+		return Login;
+	}
+	public void setLogin(String login) {
+		Login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+
+
+}
